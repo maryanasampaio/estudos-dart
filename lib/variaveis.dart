209cc28efefac98bcb988c1ef6267bf374335cd5
Maryana Sampaio <maryanacem.star2@gmail.com>
2024-01-04 22:48:43 -0300

    testes básico sobre variáveis

---
+++ lib/variaveis.dart
@@ -0,0 +1,15 @@
+//testes básicos sobre variaveis e seus tipos
+
+String palavra = 'Olá, mundo!';
+int numeroInteiro = 10;
+double numeroDecimal = 10.0;
+bool ireiSair = true;
+List<dynamic> todosOsTipos = ['Olá, mundo!', 10, 10.0, true];
+
+void main(List<String> args) {
+  print(palavra);
+  print(numeroInteiro);
+  print(numeroDecimal);
+  print(ireiSair);
+  print(todosOsTipos);
+}
